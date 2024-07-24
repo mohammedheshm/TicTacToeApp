4e5a9d5892f07f2195ef0f338fdb49e6a9b6e923
Muhamed Sultan <muhamedsultan211@gmail.com>
2024-07-24 17:27:56 +0300

    Create Auth Pages

---
+++ Client App/HomePage/src/auth/FXMLLoginController.java
@@ -0,0 +1,39 @@
+
+package auth;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import pagemanager.Navigation;
+
+
+public class FXMLLoginController implements Initializable {
+
+        
+    
+   
+      @FXML
+    private void handleSignUpButton(ActionEvent event) {
+              
+        Navigation.nextPage(event, "/auth/FXMLSignUp.fxml");
+        
+        
+    }
+   
+    
+       @FXML
+    private void handleBackHButton(ActionEvent event) {
+              
+        Navigation.nextPage(event, "/homepage/FXMLHome.fxml");
+
+    }
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
