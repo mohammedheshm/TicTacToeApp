00a1e0f7760f31b723a206fe3cf6d579f3cde865
Rawan-Mo21 <rawan.mohamed21k@gmail.com>
2024-07-24 17:32:13 +0300

    Create ipaddress page

---
+++ Client App/HomePage/src/ipaddresspage/FXMLIpAddressController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipaddresspage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class FXMLIpAddressController implements Initializable {
+
+  
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    
+    }    
+    
+}
