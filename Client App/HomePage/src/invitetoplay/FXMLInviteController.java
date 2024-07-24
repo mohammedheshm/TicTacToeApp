144406e45d8f63e0bafa4789b650357f65938c22
Rawan-Mo21 <rawan.mohamed21k@gmail.com>
2024-07-24 17:32:46 +0300

    Create invite to play page

---
+++ Client App/HomePage/src/invitetoplay/FXMLInviteController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package invitetoplay;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+/**
+ * FXML Controller class
+ *
+ * @author rawan
+ */
+public class FXMLInviteController implements Initializable {
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    private TableColumn<?, ?> playerColumn;
+
+    @FXML
+    private TableColumn<?, ?> scoreColumn;
+
+    @FXML
+    private TableColumn<?, ?> statusColumn;
+
+  
+    
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Customize column header
+        playerColumn.setText("Player");
+        playerColumn.setStyle("-fx-background-color: #D0B8A8; -fx-text-fill: #8D493A; -fx-font-weight: bold;");
+
+        scoreColumn.setText("Score");
+        scoreColumn.setStyle("-fx-background-color: #D0B8A8; -fx-text-fill: #8D493A; -fx-font-weight: bold;");
+
+        statusColumn.setText("Status");
+        statusColumn.setStyle("-fx-background-color: #D0B8A8; -fx-text-fill: #8D493A; -fx-font-weight: bold;");
+
+    }    
+    
+}
