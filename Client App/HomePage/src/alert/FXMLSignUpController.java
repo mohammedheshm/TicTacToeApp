5913864d8d1c6115b7c3379e9d4e1e989abe2178
Muhamed Sultan <muhamedsultan211@gmail.com>
2024-07-24 17:26:30 +0300

    Create Alert Dialogs

---
+++ Client App/HomePage/src/alert/FXMLSignUpController.java
@@ -0,0 +1,19 @@
+
+package alert;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class FXMLSignUpController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ Client App/HomePage/src/alert/FXMLSuccessSignUpController.java
@@ -0,0 +1,19 @@
+
+package alert;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class FXMLSuccessSignUpController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
