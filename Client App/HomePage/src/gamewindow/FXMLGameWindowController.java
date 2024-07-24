d941cdced9651ccdba157d782211f9d072bd8d33
Muhamed Sultan <muhamedsultan211@gmail.com>
2024-07-24 17:28:46 +0300

    Create Game Window Page

---
+++ Client App/HomePage/src/gamewindow/FXMLGameWindowController.java
@@ -0,0 +1,41 @@
+package gamewindow;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import pagemanager.Navigation;
+
+
+public class FXMLGameWindowController implements Initializable {
+
+  
+
+    
+    
+       @FXML
+    public void handleBackHButton(ActionEvent  event) {
+            
+        
+       Navigation.nextPage(event, "/homepage/FXMLHome.fxml");
+       
+       
+    }
+    
+        @FXML
+    public void handleScreenShootButton(ActionEvent  event) {
+            
+        
+      //Logic..............
+       
+       
+    }
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
