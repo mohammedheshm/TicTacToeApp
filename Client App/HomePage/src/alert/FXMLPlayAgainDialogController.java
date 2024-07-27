becdfbccf2802713305178453faee3079619dc21
Muhamed Sultan <muhamedsultan211@gmail.com>
2024-07-27 17:21:37 +0300

    Create Play Again Controller

--- Client App/HomePage/src/alert/FXMLPlayAgainDialogController.java
+++ Client App/HomePage/src/alert/FXMLPlayAgainDialogController.java
@@ -1,17 +1,44 @@
-
 package alert;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class FXMLPlayAgainDialogController {
+
+    @FXML
+    private Button okButton;
+    @FXML
+    private Button cancelButton;
+
+    private Stage dialogStage;
+    private boolean okClicked = false;
+
+    @FXML
+    private void initialize() {
+    }
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    public boolean isOkClicked() {
+        return okClicked;
+    }
+
+   @FXML
+private void handleOkButton() {
+    if (dialogStage != null) {
+        okClicked = true;
+        dialogStage.close();
+    } else {
+        System.out.println("dialogStage is null");
+    }
+}
 
 
-public class FXMLTiedController implements Initializable {
-
-   
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
-    
+    @FXML
+    private void handleCancelButton() {
+        dialogStage.close();
+    }
 }
