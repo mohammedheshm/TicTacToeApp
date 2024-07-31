4d3441c3cc695346220e4008e113d8fca5d7f338
Muhamed Sultan <muhamedsultan211@gmail.com>
2024-07-31 21:51:18 +0300

    Update Server Connection

--- Client App/HomePage/src/serverconnection/ServerConnect.java
+++ Client App/HomePage/src/serverconnection/ServerConnect.java
@@ -5,18 +5,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ServerConnect {
-    private static  String SERVER_ADDRESS = "localhost"; // IP 
+
+    private static String SERVER_ADDRESS = "localhost"; // IP 
     private static final int SERVER_PORT = 1529;
     private static Socket socket;
     private static PrintWriter out;
     private static BufferedReader in;
+    private static Thread listenerThread;
+    private static volatile boolean running = false;
 
     public static void makeConnectionWithServer() throws IOException {
-        socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
-        out = new PrintWriter(socket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        if (socket == null || socket.isClosed()) {
+            socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            
+            new Thread(){
+                public void run()
+                {
+                    while (running) {                        
+                        try {
+                            String msg = in.readLine();
+                            System.out.println(msg);
+                        } catch (IOException ex) {
+                            Logger.getLogger(ServerConnect.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+            };
+        }
     }
 
     public static void setSERVER_ADDRESS(String SERVER_ADDRESS) {
@@ -32,13 +53,23 @@ public class ServerConnect {
     }
 
     public static void closeConnection() throws IOException {
-        if (in != null) in.close();
-        if (out != null) out.close();
-        if (socket != null) socket.close();
+        running = false;
+        if (in != null) {
+            in.close();
+        }
+        if (out != null) {
+            out.close();
+        }
+        if (socket != null) {
+            socket.close();
+        }
+        if (listenerThread != null) {
+            listenerThread.interrupt();
+        }
     }
 
     public static boolean isConnection() throws IOException {
-        makeConnectionWithServer();
-        return socket.isConnected();
+        //makeConnectionWithServer();
+        return running;
     }
 }
